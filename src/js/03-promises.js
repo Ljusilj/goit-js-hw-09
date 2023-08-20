cac58cc67d0507857f3ddb201aa7fc5d9866daef
Ljusilj <129436912+Ljusilj@users.noreply.github.com>
2023-08-20 22:52:12 +0300

    My commit-9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
 
+const buttonElem = document.querySelector('button');
+const formElem = document.querySelector('.form');
+const inputDel = formElem.querySelector('[name="delay"]');
+const inputDelStep = formElem.querySelector('[name="step"]');
+const inputAmount = formElem.querySelector('[name="amount"]');
+
+buttonElem.addEventListener('click', onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  const initialDelay = Number(inputDel.value); 
+  const step = Number(inputDelStep.value);
+  const amount = Number(inputAmount.value);
+
+  async function createPromises() {
+    let delay = initialDelay; 
+    for (let position = 1; position <= amount; position += 1) {
+      try {
+        await createPromise(position, delay);
+        delay += step;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  createPromises();
+}
 
 function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
@@ -10,33 +40,11 @@ function createPromise(position, delay) {
         reject({ position, delay });
       }
     }, delay);
-  });
-}
-
-document.querySelector('.form').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const delayInput = parseInt(document.querySelector('input[name="delay"]').value);
-  const stepInput = parseInt(document.querySelector('input[name="step"]').value);
-  const amountInput = parseInt(document.querySelector('input[name="amount"]').value);
-
-  const promises = [];
-
-  for (let i = 1; i <= amountInput; i+=1) {
-    const currentDelay = delayInput + (i - 1) * stepInput;
-    promises.push(createPromise(i, currentDelay));
-  }
-
-  Promise.allSettled(promises).then(results => {
-    results.forEach(result => {
-      if (result.status === 'fulfilled') {
-        console.log(`✅ Fulfilled promise ${result.value.position} in ${result.value.delay}ms`);
-      } else {
-        console.log(`❌ Rejected promise ${result.reason.position} in ${result.reason.delay}ms`);
-      }
+  })
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     });
-  });
-});
-
-
-
+}
